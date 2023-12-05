c038417fbde26201827309436a70fd7050a6a7bc
JonathanEMyers <152437431+JonathanEMyers@users.noreply.github.com>
2023-12-04 21:06:10 -0600

    Add files via upload

---
+++ ProductInfo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PMS;
+
+import OMS.OrderInfo;
+import UMS.AddressInfo;
+import UMS.UserInfo;
+import java.util.*;
+
+/**
+ *
+ * @author jmyer
+ */
+public class ProductInfo {
+
+}
